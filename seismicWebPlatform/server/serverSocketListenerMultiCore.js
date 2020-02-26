3655f568354b1f50fe736f00cf00cf3f45a09988
marcomanso <marco@edgeneering.eu>
2020-02-26 17:30:18 +0000

    added JSON parsing error protection

--- seismicWebPlatform/server/serverSocketListenerMultiCore.js
+++ seismicWebPlatform/server/serverSocketListenerMultiCore.js
@@ -271,23 +271,29 @@ wsserver.mount({ httpServer: server,
       //then see if should accept connection
       
       //get id 
-      var sensorData=JSON.parse(req.httpRequest.headers['user-agent']);
-      if (typeof sensorData == "undefined") {    
-        writeLogAndConsole("log_","Cannot retrieve sensor_id information.")
+      try {
+        var sensorData=JSON.parse(req.httpRequest.headers['user-agent']);
+        if (typeof sensorData == "undefined") {    
+          writeLogAndConsole("log_","Cannot retrieve sensor_id information.")
+          rejectRequest(req);
+        }
+        var sensorkey=sensorData['sensor_id'];
+        sensor_frequency=1000.0/parseFloat(sensorData['period_ms']);
+        if (isNaN(sensor_frequency)) {
+          sensor_frequency = DEF_SENSOR_FREQUENCY;
+        }
+        sensor_range_g=parseFloat(sensorData['max_range']);
+        if (isNaN(sensor_range_g)) {
+          sensor_range_g = DEF_SENSOR_RANGE_G;
+        }
+        sensor_conversion_range=parseFloat(sensorData['conversion_scale_1g']);
+        if (isNaN(sensor_conversion_range)) {
+          sensor_conversion_range = DEF_CONVERSION_RAGE;
+        }
+      }
+      catch (e) {        
+        writeLogAndConsole("log_","Malformed JSON from headers: "+req.httpRequest.headers['user-agent'])
         rejectRequest(req);
-      }
-      var sensorkey=sensorData['sensor_id'];
-      sensor_frequency=1000.0/parseFloat(sensorData['period_ms']);
-      if (isNaN(sensor_frequency)) {
-        sensor_frequency = DEF_SENSOR_FREQUENCY;
-      }
-      sensor_range_g=parseFloat(sensorData['max_range']);
-      if (isNaN(sensor_range_g)) {
-        sensor_range_g = DEF_SENSOR_RANGE_G;
-      }
-      sensor_conversion_range=parseFloat(sensorData['conversion_scale_1g']);
-      if (isNaN(sensor_conversion_range)) {
-        sensor_conversion_range = DEF_CONVERSION_RAGE;
       }
      
      //for now do not use certificates //msg_signed_b64=decodeURIComponent(req.httpRequest.headers['x-custom']);
