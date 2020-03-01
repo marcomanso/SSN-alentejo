b391491f2ec4c46b90c9fda15d6c9f2cbe52bac0
marcomanso <marco@edgeneering.eu>
2020-03-01 16:07:57 +0000

    added support to binary data

---
+++ seismicWebPlatform/utils/datautils.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  getLongValueFrom32BitData: function (data) {
+    return data.readInt32BE();
+    //return (data.readInt(0)<<16 + data.readInt(1)<<8 + data.readInt(2));
+  },
+  
+}
