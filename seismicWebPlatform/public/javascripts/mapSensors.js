9cc459a352f8055d9b6294268a3a74bf99fe202a
marcomanso <marco@edgeneering.eu>
2020-03-06 11:13:10 +0000

    test statistical functions - EVENT

--- seismicWebPlatform/public/javascripts/mapSensors.js
+++ seismicWebPlatform/public/javascripts/mapSensors.js
@@ -194,7 +194,10 @@ function newSensorEventMessage(sensorevent_msg) {
   if (typeof sensorEventMap.get(sensorevent_msg.sensorid) === 'undefined') {
     sensorEventMap.set(sensorevent_msg.sensorid, new Map());
   }
-  if ( sensorevent_msg.time_end_ms == 0 ) {
+  if ( sensorevent_msg.time_end_ms === 0 ) {
+
+console.log("--is EVENT");
+
     setSensorAsMoving(sensorevent_msg.sensorid, sensorevent_msg.d_accel_rms);
     sensorEventMap.get(sensorevent_msg.sensorid).set(eventData.time_start_ms, sensorevent_msg);
   }
