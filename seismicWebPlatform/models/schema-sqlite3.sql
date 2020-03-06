964b236c99c757c094c7bfd4f56a344ce0c72c92
marcomanso <marco@edgeneering.eu>
2020-03-06 19:50:27 +0000

    add event based functions

--- seismicWebPlatform/models/schema-sqlite3.sql
+++ seismicWebPlatform/models/schema-sqlite3.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS sensors (
   data_URL       VARCHAR(100) NOT NULL
 );
 
-DROP TABLE events IF EXISTS;
+DROP TABLE events;
+
 CREATE TABLE IF NOT EXISTS events (
   sensorkey      TEXT NOT NULL,
   time_start_ms  INTEGER NOT NULL,
-  PRIMARY KEY (sensorkey, time_start_ms),
   time_end_ms    INTEGER,
   d_accel_x      DOUBLE,
   d_accel_y      DOUBLE,
@@ -26,5 +26,6 @@ CREATE TABLE IF NOT EXISTS events (
   max_accel_y    DOUBLE,
   max_accel_z    DOUBLE,
   max_accel_rms  DOUBLE,
-  stddev_rms     DOUBLE
+  stddev_rms     DOUBLE,
+  PRIMARY KEY (sensorkey, time_start_ms)
 );
